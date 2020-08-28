b8e48ccd83dc2c8324c0bab5236cd6d3eaed749e
eldhosempeter07 <eldhosempeter07@gmail.com>
2020-08-28 21:22:18 +0530

    Design the calculator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Buttons from './components/Buttons';
+import './components/css/style.css'
 
-function App() {
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      previous:[],
+      current:''   
+    }
+  }
+  reset=()=>{
+    this.setState({
+      current:0
+    })
+  }
+  
+  addToCurrent=(symbol)=>{
+    this.setState({
+      current:this.state.current+symbol
+    });
+  }
+render(){
+  const buttons=[
+    {symbol:'C',cols:3,action:this.reset},
+    {symbol:'/',cols:1,action:this.addToCurrent},
+    {symbol:'7',cols:1,action:this.addToCurrent},
+    {symbol:'8',cols:1,action:this.addToCurrent},
+    {symbol:'9',cols:1,action:this.addToCurrent},
+    {symbol:'X',cols:1,action:this.addToCurrent},
+    {symbol:'4',cols:1,action:this.addToCurrent},
+    {symbol:'5',cols:1,action:this.addToCurrent},
+    {symbol:'6',cols:1,action:this.addToCurrent},
+    {symbol:'-',cols:1,action:this.addToCurrent},
+    {symbol:'1',cols:1,action:this.addToCurrent},
+    {symbol:'2',cols:1,action:this.addToCurrent},
+    {symbol:'3',cols:1,action:this.addToCurrent},
+    {symbol:'+',cols:1,action:this.addToCurrent},
+    {symbol:'0',cols:2,action:this.addToCurrent},
+    {symbol:'.',cols:1,action:this.addToCurrent},
+    {symbol:'=',cols:1,action:this.addToCurrent},
+  
+  ]
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input className='result' value={this.state.current}/>
+      {buttons.map((button,i)=>{
+        return<Buttons key={i} symbol={button.symbol} cols ={button.cols} action={(symbol)=>button.action(symbol)}/>
+      })}
     </div>
   );
 }
-
+}
 export default App;
---
+++ src/components/Buttons.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+class Buttons extends Component {
+    
+    
+    render() {
+        return (
+            <div className={`columns-${this.props.cols}`}>   
+                <button className='calc-button' onClick={()=>this.props.action(this.props.symbol)} >{this.props.symbol}</button>
+            </div>
+        )
+    }
+}
+
+export default Buttons
